9b86c88ca3f3def5ebc9be8d2eca57ea89221cc0
lucamazzone <luca.g.mazzone@gmail.com>
2017-02-17 14:18:44 +0100

    Add files via upload

--- MAIN.f95
+++ MAIN.f95
@@ -1,33 +1,33 @@
-program MAIN 
+  program MAIN 
   use params
   use library
   use solution_lib
-!  USE BLAS_SPARSE
+!  use invert_numeric
+  use invert_direct
+
 
   implicit none
   
 !!!!!!!!!!!!!
 integer, parameter :: snum = 2
+integer :: zct,aggregate,curr_state
+integer :: iii,jjj,kkk,rc
+integer :: politics(vecsize,vecsize,Zsize),politics0(vecsize,vecsize,Zsize)
+
 double precision  :: logS(snum), Sprob(snum,snum), SS(snum)
 double precision  :: logz(Zsize), Zprob(Zsize,Zsize), zeta(Zsize,snum)
 double precision  :: s(Zsize,1),s_alt(Zsize,1)
-integer :: zct,aggregate,curr_state
+
 double precision :: lgrid(vecsize,vecsize),bgrid(vecsize,vecsize)
 double precision :: lgrid_int(vecinterp,vecinterp),bgrid_int(vecinterp,vecinterp)
 double precision :: weights(nsimp+1), nodes(nsimp+1)
 double precision :: weights_b(nsimp+1), nodes_b(nsimp+1)
-double precision :: qfun(vecsize,vecsize)
-double precision :: Ybig,Cons,Cons_1,Nbig_1,Ybig_1,zeta_tomorrow(Zsize,1)
+double precision :: qfun(vecsize,vecsize),Q, v(vecsize,vecsize,Zsize),qq(vecsize,vecsize,Zsize)
+double precision :: Ybig,Cons,Cons_1,Nbig_1,Ybig_1,zeta_tomorrow(Zsize,1),Nbig,wage
 
 !!!!!!!!!!!!!
 
-integer :: pt(64)
-integer :: mtype, solver,error
-integer :: iparm(64)
-real(kind=8) :: dparm(64)
 
-
-!!!!!!!!!!!!!
 
 !! construct stochastic process
 
@@ -62,14 +62,44 @@ call qsimpweightsnodes(stepl,lmax,nsimp,weights,nodes)
 call qsimpweightsnodes(stepb,bmax,nsimp,weights_b,nodes_b)
 
 call  q_fun(qfun,Ybig,Cons,Cons_1,Nbig_1,Ybig_1,lgrid,&
- bgrid,zeta_tomorrow,Zprob,curr_state,vecsize,Zsize,alpha,beta,gamma,eta,chi)
+ bgrid,zeta_tomorrow,Zprob,curr_state,vecsize,Zsize,alpha,beta,gamma,eta,chi,Q)
+!! remember this has to be called Zsize times, one for each curr_state
+
+do kkk=1,Zsize
+   do jjj=1,vecsize
+      do iii=1,vecsize
+      politics0(iii,jjj,kkk) = iii + (jjj-1)*vecsize   
+      end do
+   end do
+end do
+
+!!!!!!! experiment for valfun
+ Q = 1.0
+ qq = beta
+ Cons= 0.6
+ Nbig = 0.7
+ Ybig = 0.72
+ !zeta1 = zeta(:,1)
+ !tv = 0.0
+ 
+ wage = (Cons**eta)*(Nbig**chi)
+ 
 
 
-   mtype = 11
-    solver = 0
+ ! external DGETRI
+
+! call valuefun(vecsize,Zsize,politics0,Zprob,Nbig,Ybig,Cons,Q,qq,zeta,wage,beta,kappa,&
+!           gamma,alpha,bgrid,lgrid,v)
+
+ 
+!print*, v
+
+   
+
+
     
 
-      call pardisoinit(pt, mtype, solver, iparm, dparm, error)
+  !    call pardisoinit(pt, mtype, solver, iparm, dparm, error)
   !    IF (error .NE. 0) THEN
   !      IF (error.EQ.-10 ) WRITE(*,*) 'No license file found'
   !      IF (error.EQ.-11 ) WRITE(*,*) 'License is expired'
@@ -88,53 +118,73 @@ call  q_fun(qfun,Ybig,Cons,Cons_1,Nbig_1,Ybig_1,lgrid,&
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     contains
 
-      subroutine valuefun(vecsize,Zsize,politics,Zprob,outt)
+      subroutine valuefun(vecsize,Zsize,politics,Zprob,Nbig,Ybig,Cons,Q,qq,zeta,wage,beta,kappa,&
+           gamma,alpha,bgrid,lgrid,v)
+integer, intent(in) :: vecsize, Zsize, politics(vecsize,vecsize,Zsize)
+double precision, intent(in) :: Nbig, Ybig, Cons,Q,zeta(Zsize),wage,Zprob(Zsize,Zsize)
+double precision, intent(in) :: beta,kappa,gamma,alpha,lgrid(vecsize,vecsize),bgrid(vecsize,vecsize)
+double precision, intent(out) ::  v(vecsize,vecsize,Zsize)
 
-  integer,intent(in) :: vecsize,Zsize
-  double precision, intent(in) :: politics(vecsize,vecsize,Zsize),Zprob(Zsize,Zsize) 
-  integer, intent(out) :: outt
-  double precision:: polco(vecsize*vecsize)
-  integer :: G, ISTAT,N,i,A,j,Zsq
-  integer, DIMENSION(:), ALLOCATABLE :: INDX,JNDX
-  REAL(KIND=8), DIMENSION(:), ALLOCATABLE :: VAL
 
-  N=vecsize**2
-  Zsq=Zsize**2
-   ALLOCATE (VAL(N*Zsq),INDX(N*Zsq),JNDX(N*Zsq))
+!! other declarations
+double precision :: T(Zsize*vecsize**2,Zsize*vecsize**2), EYE(Zsize*vecsize**2,Zsize*vecsize**2)
+double precision :: BUM(Zsize*vecsize**2,Zsize*vecsize**2),MUB(Zsize*vecsize**2,Zsize*vecsize**2)
+double precision :: x(vecsize,vecsize,Zsize), ut0(vecsize,vecsize,Zsize), ut(Zsize*vecsize**2)
+double precision :: zeta1(Zsize), tv(Zsize*vecsize**2),res(Zsize*vecsize**2),qq(vecsize,vecsize,Zsize)
+integer::  polco(vecsize**2), iii,jjj,kkk,lpol,bpol,iter
 
+
+!!!!!!!!!!!!!
+T = 0.0
+EYE = 0.0
+
+do kkk=1,Zsize
+   polco = reshape(politics(:,:,kkk),(/vecsize**2/))
    
-   CALL duscr_begin( N*Zsize, N*Zsize, G, ISTAT)
+  do jjj=1,Zsize
+      do iii=1,vecsize**2
+         T( iii + (kkk-1)*vecsize**2 , polco(iii)+ (jjj-1)*vecsize**2 ) = &
+              Zprob(kkk,jjj)
+      end do
+  end do
+end do
 
-   do j=1,Zsize
-    polco = reshape(politics(:,:,j),(/1/))         
-    do i = 1,N*Zsize      
-       INDX(i+(j-1)*N) = i
-       JNDX(i+(j-1)*N) = polco(i)+(j-1)*N
-       VAL(i+(j-1)*N) = Zprob((i-1)/j +1,j)
-   end do
-   end do
 
-   CALL duscr_insert_entries(G, VAL, INDX, JNDX, ISTAT)
+ do iii=1,vecsize**2
+    EYE(iii,iii)= 1.0
+ end do
 
-    ! ut = [];
+ BUM = EYE - (1-kappa)*Q*beta*T
 
- !     for curr_state = 1:5;
-
-!    q = qq(:,:,curr_state);
-!    x(:,:,curr_state) = zeta1(curr_state)*Ybig^(1/gamma)*lgrid.^(alpha-alpha/gamma)
-!    - Nbig^chi*Ybig^eta*lgrid - bgrid;
-!    ut0 = kappa*(x(:,:,curr_state) +
-!    q(politics(:,:,curr_state)).*bgrid(politics(:,:,curr_state)));
-!    ut = [ut, ut0];
-!            end
-
-!    ciao =   reshape(ut,vecsize,vecsize,5);
-!    tv = (speye(5*vecsize^2) - Q*beta*trans)\ciao(:);
-
-    !  CALL USPG(A,blas_num_nonzeros,outt)
  
+ do curr_state = 1,Zsize
+    do jjj = 1,vecsize       
+       do iii = 1,vecsize
     
- 
+ x(iii,jjj,curr_state) = zeta(curr_state)*(Ybig**(1/gamma))*lgrid(iii,jjj)**(alpha-alpha/gamma)-&
+      wage*lgrid(iii,jjj) - bgrid(iii,jjj)
+ lpol =  mod(politics(iii,jjj,curr_state)-1,vecsize)+1
+ bpol =  (politics(iii,jjj,curr_state)+vecsize-1)/vecsize
+ ut0(iii,jjj,curr_state) = kappa*x(iii,jjj,curr_state)+qq(lpol,bpol,curr_state)*bgrid(lpol,bpol)
+
+       end do
+    end do
+end do
+
+
+ut = reshape(ut0,(/Zsize*vecsize**2/))
+
+
+! OMEGA Relaxation coefficient = 1.0
+!tv = -1.0
+!call seidel(3,Zsize*vecsize**2,BUM,ut,1.0_8,tv,res,iter,rc)
+!v = reshape(tv,(/vecsize,vecsize,Zsize/))
+
+
+call inverse(BUM,MUB,Zsize*vecsize**2)
+
+
+
  end subroutine valuefun
 
     end program MAIN
