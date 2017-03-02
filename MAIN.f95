ed3d723fa0419bf5784c834c715c4c72c0e7cf70
Luca Mazzone <mazzonel@hpc.cm.cluster>
2017-03-02 16:02:12 +0100

    just to

--- MAIN.f95
+++ MAIN.f95
@@ -25,16 +25,24 @@ double precision :: Ybig,Cons,Cons_1,Nbig_1,Ybig_1,zeta_tomorrow(Zsize,1),Nbig,w
 
 !!
 
-integer :: polco(Zsize*vecsize**2),ia(1+Zsize*vecsize**2), nonzeros
-integer,dimension(:), allocatable :: ja
-real*8,dimension(:), allocatable :: a
+integer :: nn=Zsize*vecsize**2
+double precision :: value0(vecsize,vecsize,Zsize),vvalue(vecsize,vecsize,Zsize),zeta1(Zsize)
 
-integer :: pt(64)
-integer :: iparm(64)
-real*8 :: dparm(64)
-integer :: mtype,solver,error,nn,lpol,bpol,phase,msglvl,mnum=1,idum,nrhs,maxfct=1
-real*8 :: x(vecsize,vecsize,Zsize),ut0(vecsize,vecsize,Zsize),vv(Zsize*vecsize**2), ut(Zsize*vecsize**2),zeta1(Zsize)
-real*8 :: ddum
+
+
+
+
+
+!integer :: polco(Zsize*vecsize**2),ia(1+Zsize*vecsize**2), nonzeros
+!integer,dimension(:), allocatable :: ja
+!real*8,dimension(:), allocatable :: a
+
+!integer :: pt(64)
+!integer :: iparm(64)
+!real*8 :: dparm(64)
+!integer :: mtype,solver,error,nn,lpol,bpol,phase,msglvl,mnum=1,idum,nrhs,maxfct=1
+!real*8 :: x(vecsize,vecsize,Zsize),ut0(vecsize,vecsize,Zsize),vv(Zsize*vecsize**2), ut(Zsize*vecsize**2),zeta1(Zsize)
+!real*8 :: ddum
 
 
 
@@ -70,11 +78,6 @@ call creategrid(lmax,bmax,stepl,stepb,vecinterp,lgrid_int,bgrid_int)
 call qsimpweightsnodes(stepl,lmax,nsimp,weights,nodes)
 call qsimpweightsnodes(stepb,bmax,nsimp,weights_b,nodes_b)
 
-do curr_state = 1,Zsize
-call  q_fun(qfun,Ybig,Cons,Cons_1,Nbig_1,Ybig_1,lgrid,&
-&  bgrid,zeta_tomorrow,Zprob,curr_state,vecsize,Zsize,alpha,beta,gamma,eta,chi,Q)
-qq(:,:,curr_state) = qq(:,:)
-end do
 
 do kkk=1,Zsize
    do jjj=1,vecsize
@@ -86,137 +89,39 @@ end do
 
 
 !!!!!!! experiments for valfun
-Q = 1.0
 
-polco =reshape(politics0(:,:,:),(/Zsize*vecsize**2/))
-
-
-ia(1) = 1
-
-do jjj = 1,Zsize
-    do iii = 1,vecsize**2
-	if (polco(iii+ (jjj-1)*vecsize**2).EQ.iii)  then
-	ia( iii+1 + (jjj-1)*vecsize**2  ) = ia(iii + (jjj-1)*vecsize**2 ) + 1 + Zsize - 1
-	else 
-	ia( iii+1 + (jjj-1)*vecsize**2  ) = ia(iii + (jjj-1)*vecsize**2 ) + 1 + Zsize
-	end if
-    end do
-end do
-
-nonzeros = ia(1+Zsize*vecsize**2)-1
-
-allocate( ja(nonzeros))
-allocate( a(nonzeros))
-
-
-
-do iii=1,Zsize*vecsize**2
-    if      ( mod(iii-1,vecsize**2)+1  < polco(iii) ) then
-	ja(ia(iii)) = iii + (jjj-ia(iii))*vecsize**2
-	do jjj = ia(iii)+1,ia(iii+1)-1
-	    ja(jjj) = polco(iii) + (jjj-ia(iii))*vecsize**2
-	end do
-    else if ( mod(iii-1,vecsize**2)+1 > polco(iii) ) then
-	do jjj = ia(iii),ia(iii+1)-2
-	    ja(jjj) = polco(iii) + (jjj-ia(iii))*vecsize**2
-	end do
-	ja(ia(iii+1)-1) = iii + (jjj-ia(iii))*vecsize**2
-    else
-	do jjj = ia(iii),ia(iii+1)-1
-	    ja(jjj) = polco(iii) + (jjj-ia(iii))*vecsize**2
-	end do
-    end if
-end do
-
-
-
-do iii=1,Zsize*vecsize**2
-    if      (mod(iii-1,vecsize**2)+1 < polco(iii)) then
-	a(ia(iii)) = 1.0
-	do jjj = ia(iii)+1,ia(iii+1)-1
-	    a(jjj) = -beta*Q*(1-kappa)*Zprob ( (iii+vecsize**2-1)/(vecsize**2),jjj-ia(iii)-1)
-	end do
-    else if (mod(iii-1,vecsize**2)+1 > polco(iii)) then
-	do jjj = ia(iii),ia(iii+1)-2
-	    a(jjj) = -beta*Q*(1-kappa)*Zprob( (iii+vecsize**2-1)/(vecsize**2),jjj-ia(iii))
-	end do
-	a(ia(iii+1)-1) = 1.0
-    else
-	do jjj= ia(iii),ia(iii+1)-1
-	    a(jjj) = -beta*Q*(1-kappa)*Zprob( (iii+vecsize**2-1)/(vecsize**2),jjj-ia(iii))
-	end do
-	a(ia(iii)+(iii+vecsize**2-1)/(vecsize**2)-1) = &
-	& 1-beta*Q*(1-kappa)*Zprob((iii+vecsize**2-1)/(vecsize**2),(iii+vecsize**2-1)/(vecsize**2))
-    end if
-end do
-
-
-
-solver = 0
-mtype = 11
-nn = Zsize*vecsize**2
-
-call pardisoinit(pt,mtype,solver,iparm,dparm,error)
-
-iparm(3)=1
-
-call pardiso_chkmatrix(mtype,nn,a,ia,ja,error)
-
-
-
- qq = beta
  Cons= 0.6
  Nbig = 0.7
  Ybig = 0.72
  zeta1 = zeta(:,1)
- !tv = 0.0
  wage = (Cons**eta)*(Nbig**chi)
-
-do kkk = 1,Zsize
-    do jjj = 1,vecsize
-	do iii = 1,vecsize
-	    x(iii,jjj,kkk) = zeta1(kkk)*(Ybig**(1/gamma))*lgrid(iii,jjj)**(alpha-alpha/gamma)- &
-	    & wage*lgrid(iii,jjj) - bgrid(iii,jjj)
-	    lpol = mod(politics(iii,jjj,kkk)-1,vecsize)+1
-	    bpol = (politics(iii,jjj,kkk)+vecsize-1)/vecsize
-	    ut0(iii,jjj,kkk) = kappa*x(iii,jjj,kkk)+qq(lpol,bpol,kkk)*bgrid(lpol,bpol)
-	end do
-    end do
+ 
+ 
+ 
+ 
+do curr_state = 1,Zsize
+call  q_fun(qfun,Ybig,Cons,Cons_1,Nbig_1,Ybig_1,lgrid,&
+&  bgrid,zeta_tomorrow,Zprob,curr_state,vecsize,Zsize,alpha,beta,gamma,eta,chi,Q)
+qq(:,:,curr_state) = qfun(:,:)
 end do
 
-ut = reshape(ut0,(/Zsize*vecsize**2/))
-
-
-!call pardiso_chkvec(nn,1,ut,error)
-
  
-!call  pardiso_printstats(mtype,nn,a,ia,ja,1,ut,error)
+
+!do kkk = 1,Zsize
+!    do jjj = 1,vecsize
+!	do iii = 1,vecsize
+!	    x(iii,jjj,kkk) = zeta1(kkk)*(Ybig**(1/gamma))*lgrid(iii,jjj)**(alpha-alpha/gamma)- &
+!	    & wage*lgrid(iii,jjj) - bgrid(iii,jjj)
+!	    lpol = mod(politics(iii,jjj,kkk)-1,vecsize)+1
+!	    bpol = (politics(iii,jjj,kkk)+vecsize-1)/vecsize
+!	    ut0(iii,jjj,kkk) = kappa*x(iii,jjj,kkk)+qq(lpol,bpol,kkk)*bgrid(lpol,bpol)
+!	end do
+!    end do
+!end do
+
+!ut = reshape(ut0,(/Zsize*vecsize**2/))
 
 
-phase = 11  ! only reordering and symbolic factorization
-msglvl = 1  ! with statistical information
-
-!call pardiso(pt,maxfct,mnum,mtype,phase,nn,a,ia,ja,idum,nrhs,iparm,msglvl,ddum,ddum,error,dparm)
-
-phase = 22  ! only factorization
-
-!call pardiso(pt,maxfct,mnum,mtype,phase,nn,a,ia,ja,idum,nrhs,iparm,msglvl,ddum,ddum,error,dparm)
-
-phase = 33 ! only solve
-iparm(8) = 1 ! max number of iterative refinement steps
-iparm(2) = 0 ! try
-
-!call pardiso(pt,maxfct,mnum,mtype,phase,nn,a,ia,ja,idum,nrhs,iparm,msglvl,ut,vv,error,dparm)
-
-!phase = 33
-!iparm(8) = 1
-!iparm(12) = 1
-
-
-!call pardiso(pt,maxfct,mnum,mtype,phase,nn,a,ia,ja,idum,nrhs,iparm,msglvl,ut,vv,error,dparm)
-
-phase = -1
-!call pardiso(pt,maxfct,mnum,mtype,phase,nn,ddum,idum,idum,idum,nrhs,iparm,msglvl,ddum,ddum,error,dparm)
 
 
 
