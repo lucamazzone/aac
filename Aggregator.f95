59734b0efd2c9d15ede69137a6886783c466a3d3
Luca Mazzone <mazzonel@hpc.cm.cluster>
2017-03-17 17:44:56 +0100

    aggregator

--- Aggregator.f95
+++ Aggregator.f95
@@ -1,18 +1,21 @@
 program Aggregator
 
-use parameters
-use solution_lib
-use library
+!use parameters
+!use solution_lib
+!use library
 
 implicit none
 
-double precision :: intvecmat(2,Zsize)
+double precision :: intvecmat(2,10)
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-open(10002, file="intvecmat.txt")! read in values
+open(10002, file="intvectors.txt")! read in values
 read(10002,*) intvecmat
 close(10002)
+
+
+print*, intvecmat(1,:)
 
 
 
