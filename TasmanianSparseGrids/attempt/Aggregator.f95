756f8ca0d2b8e487e49c8ad3e6c0c2ff4bea5f09
lucamazzone <luca.g.mazzone@gmail.com>
2017-04-16 17:41:47 +0200

    final aggregator

--- TasmanianSparseGrids/attempt/Aggregator.f95
+++ TasmanianSparseGrids/attempt/Aggregator.f95
@@ -1,9 +1,39 @@
 module Aggregator
+implicit none
+
+  double precision, parameter :: alpha = 0.7 !labor productivity
+  double precision, parameter :: beta = 0.985 ! hh discount factor => r = 2,67% 
+  double precision, parameter :: eta = 1 ! hh IES
+  double precision, parameter :: chi = 0.5  ! Fritsch labor elasticity
+  double precision, parameter :: gamma = 7.7 ! elasticity of subs b/w goods
+  double precision, parameter :: rhoz = 0.7 ! serial corr of idiosync shocks
+  double precision, parameter :: rhosigma = 0.85 ! serial corr of unc shocks
+  double precision, parameter :: kappa = 0.4 ! Jensen effect
+  double precision, parameter :: phi = 0.13 ! std of unc shocks
+  double precision, parameter :: musigma = 0.18 ! mean of unc process
+  double precision, parameter :: csi = 0.5 ! entry costs
+
+  double precision, parameter :: nstdevz = 1.0  ! stuff for tauchen
+
+  integer, parameter :: Zsize = 10 ! colsize of idiosync shock matrix
+  integer, parameter :: vecsize = 40 ! colsize of lgrid and bgrid
+  integer, parameter :: vecinterp = 150 ! size of interpolated version
+  integer, parameter :: snum = 2 ! high, low uncertainty
+  
+  integer, parameter :: nsimp = 100 ! number of Simpson quadnodes (ntb even!)
+  integer, parameter :: momnum = 5 ! number of reference moments to be used
+
+  ! parameters for the grid of the firm problem
+
+  double precision, parameter :: lmax = 1.3871 ! maxval of labor grid
+  double precision, parameter :: bmax  = 0.5 ! maxval of debt grid
+  double precision, parameter :: stepl = lmax/vecsize ! distance b/w gridpoints
+  double precision, parameter :: stepb = bmax/vecsize ! distance b/w gridpoints
 
 ! change end program into end module at the end
-use params
-use solution_lib
-use library
+!use params
+!use solution_lib
+!use library
 
 !double precision :: points(3)
 !double precision :: pred(3)
@@ -20,6 +50,8 @@ use library
 
 
 contains
+
+
 subroutine mapping_inverse(points,aggregate,pred)
 
 implicit none
@@ -711,5 +743,655 @@ end subroutine convertpolicy3
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+subroutine  q_fun(qfun,Ybig,Cons,Cons_1,Nbig_1,Ybig_1,lgrid,&
+&    bgrid,zeta_tomorrow,Zprob,curr_state,vecsize,Zsize,alpha,beta,gamma,eta,chi,Q)
+
+! inputs and outputs
+integer, intent(in) :: vecsize, curr_state,Zsize
+double precision, intent(in) :: Ybig, Cons, Cons_1, Nbig_1, Ybig_1, zeta_tomorrow(Zsize,1), &
+&     lgrid(vecsize,vecsize), bgrid(vecsize,vecsize), Zprob(Zsize,Zsize), &
+&     alpha,beta,gamma,eta,chi
+double precision, intent(out) :: qfun(vecsize,vecsize),Q
+! other declarations
+double precision :: bmax, p(vecsize,vecsize,Zsize), profit(vecsize,vecsize,Zsize),&
+&     repayment(vecsize,vecsize,Zsize),wage
+integer :: ii,jj,kk
+
+bmax = bgrid(vecsize,vecsize)
+Q =(Cons/Cons_1)**(eta)
+wage = Nbig_1**chi * Ybig_1**eta
+
+
+
+do ii = 1,Zsize
+
+   p(:,:,ii) = zeta_tomorrow(ii,1)*(Ybig_1/lgrid**alpha)**(1/gamma)
+   do jj = 1,vecsize
+      do kk = 1,vecsize
+         profit(kk,jj,ii) = p(kk,jj,ii)*(lgrid(kk,jj)**alpha) - wage*lgrid(kk,jj)
+      end do
+   end do
+repayment(:,:,ii) = (profit(:,:,ii)-bgrid)/bmax;
+
+end do
+
+
+
+do ii = 1,Zsize
+   do jj = 1,vecsize
+      do kk = 1,vecsize
+         
+         if (repayment(kk,jj,ii) .LT. 0.0) then
+         repayment(kk,jj,ii) = 0.0
+         elseif (repayment(kk,jj,ii) .GT. 1.0) then
+            repayment(kk,jj,ii) = 1.0
+         end if   
+      end do
+   end do
+end do
+
+
+qfun = repayment(:,:,1)*0.0
+
+do ii = 1,Zsize;  ! 10  = # of discretized idyiosinc shocks
+qfun = qfun + beta*Q*Zprob(ii,curr_state)*repayment(:,:,ii);
+end do
+
+!qprice(qprice>1) = 1;
+!qprice(qprice<0) = 0;
+
+
+do jj = 1,vecsize
+   do kk = 1,vecsize         
+         if (qfun(kk,jj) .LT. 0.0) then
+         qfun(kk,jj) = 0.0
+         elseif (qfun(kk,jj) .GT. 1.0) then
+         qfun(kk,jj) = 1.0
+         end if   
+   end do
+end do
+
+    
+end subroutine  q_fun
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+function r8vec_bracket5 ( nd, xd, xi )
+
+!*****************************************************************************80
+!
+!! R8VEC_BRACKET5 brackets data between successive entries of a sorted R8VEC.
+!
+!  Discussion:
+!
+!    We assume XD is sorted.
+!
+!    If XI is contained in the interval [XD(1),XD(N)], then the returned 
+!    value B indicates that XI is contained in [ XD(B), XD(B+1) ].
+!
+!    If XI is not contained in the interval [XD(1),XD(N)], then B = -1.
+!
+!    This code implements a version of binary search which is perhaps more
+!    understandable than the usual ones.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    14 October 2012
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) ND, the number of data values.
+!
+!    Input, real ( kind = 8 ) XD(N), the sorted data.
+!
+!    Input, real ( kind = 8 ) XD, the query value.
+!
+!    Output, integer ( kind = 4 ) R8VEC_BRACKET5, the bracket information.
+!
+  implicit none
+
+  integer ( kind = 4 ) nd
+
+  integer ( kind = 4 ) b
+  integer ( kind = 4 ) l
+  integer ( kind = 4 ) m
+  integer ( kind = 4 ) r
+  integer ( kind = 4 ) r8vec_bracket5
+  real ( kind = 8 ) xd(nd)
+  real ( kind = 8 ) xi
+
+  if ( xi < xd(1) .or. xd(nd) < xi ) then
+
+    b = -1
+
+  else
+
+    l = 1
+    r = nd
+
+    do while ( l + 1 < r )
+      m = ( l + r ) / 2
+      if ( xi < xd(m) ) then
+        r = m
+      else
+        l = m
+      end if
+    end do
+
+    b = l
+
+  end if
+
+  r8vec_bracket5 = b
+
+  return
+end
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+function r8_huge ( )
+
+!*****************************************************************************80
+!
+!! R8_HUGE returns a very large R8.
+!
+!  Discussion:
+!
+!    The value returned by this function is intended to be the largest
+!    representable real value.
+!
+!    FORTRAN90 provides a built-in routine HUGE ( X ) that
+!    can return the maximum representable number of the same datatype
+!    as X, if that is what is really desired.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    27 September 2014
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Output, real ( kind = 8 ) R8_HUGE, a "huge" value.
+!
+  implicit none
+
+  real ( kind = 8 ) r8_huge
+
+  r8_huge = 1.79769313486231571D+308
+
+  return
+end function r8_huge
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+subroutine pwl_interp_2d( nxd, nyd, xd, yd, zd, ni, xi, yi, zi )
+
+!*****************************************************************************80
+!
+!! PWL_INTERP_2D: piecewise linear interpolant to data defined on a 2D grid.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    14 October 2012
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) NXD, NYD, the number of X and Y data values.
+!
+!    Input, real ( kind = 8 ) XD(NXD), YD(NYD), the sorted X and Y data.
+!
+!    Input, real ( kind = 8 ) ZD(NXD,NYD), the Z data.
+!
+!    Input, integer ( kind = 4 ) NI, the number of interpolation points.
+!
+!    Input, real ( kind = 8 ) XI(NI), YI(NI), the coordinates of the
+!    interpolation points.
+!
+!    Output, real ( kind = 8 ) ZI(NI), the value of the interpolant.
+!
+  implicit none
+
+  integer ( kind = 4 ) ni
+  integer ( kind = 4 ) nxd
+  integer ( kind = 4 ) nyd
+
+  real ( kind = 8 ) alpha
+  real ( kind = 8 ) beta
+  real ( kind = 8 ) det
+  real ( kind = 8 ) dxa
+  real ( kind = 8 ) dxb
+  real ( kind = 8 ) dxi
+  real ( kind = 8 ) dya
+  real ( kind = 8 ) dyb
+  real ( kind = 8 ) dyi
+  real ( kind = 8 ) gamma
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ) j
+  integer ( kind = 4 ) k
+!  real ( kind = 8 ) r8_huge
+!  integer ( kind = 4 ) r8vec_bracket5
+  real ( kind = 8 ) xd(nxd)
+  real ( kind = 8 ) xi(ni)
+  real ( kind = 8 ) yd(nyd)
+  real ( kind = 8 ) yi(ni)
+  real ( kind = 8 ) zd(nxd,nyd)
+  real ( kind = 8 ) zi(ni)
+
+  do k = 1, ni
+
+    i = r8vec_bracket5 ( nxd, xd, xi(k) )
+    if ( i == -1 ) then
+      zi(k) = r8_huge ( )
+      cycle
+    end if
+
+    j = r8vec_bracket5 ( nyd, yd, yi(k) )
+    if ( j == -1 ) then
+      zi(k) = r8_huge ( )
+      cycle
+    end if
+
+    if ( yi(k) < yd(j+1) &
+    &  + ( yd(j) - yd(j+1) ) * ( xi(i) - xd(i) ) / ( xd(i+1) - xd(i) ) ) then
+
+      dxa = xd(i+1) - xd(i)
+      dya = yd(j)   - yd(j)
+
+      dxb = xd(i)   - xd(i)
+      dyb = yd(j+1) - yd(j)
+
+      dxi = xi(k)   - xd(i)
+      dyi = yi(k)   - yd(j)
+
+      det = dxa * dyb - dya * dxb
+
+      alpha = ( dxi * dyb - dyi * dxb ) / det
+      beta =  ( dxa * dyi - dya * dxi ) / det
+      gamma = 1.0D+00 - alpha - beta
+
+      zi(k) = alpha * zd(i+1,j) + beta * zd(i,j+1) + gamma * zd(i,j)
+
+    else
+
+      dxa = xd(i)   - xd(i+1)
+      dya = yd(j+1) - yd(j+1)
+
+      dxb = xd(i+1) - xd(i+1)
+      dyb = yd(j)   - yd(j+1)
+
+      dxi = xi(k)   - xd(i+1)
+      dyi = yi(k)   - yd(j+1)
+
+      det = dxa * dyb - dya * dxb
+
+      alpha = ( dxi * dyb - dyi * dxb ) / det
+      beta =  ( dxa * dyi - dya * dxi ) / det
+      gamma = 1.0D+00 - alpha - beta
+
+      zi(k) = alpha * zd(i,j+1) + beta * zd(i+1,j) + gamma * zd(i+1,j+1)
+
+    end if
+
+  end do
+
+  return
+
+end subroutine pwl_interp_2d
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+subroutine inverse(a,c,n)
+!============================================================
+! Inverse matrix
+! Method: Based on Doolittle LU factorization for Ax=b
+! Alex G. December 2009
+!-----------------------------------------------------------
+! input ...
+! a(n,n) - array of coefficients for matrix A
+! n      - dimension
+! output ...
+! c(n,n) - inverse matrix of A
+! comments ...
+! the original matrix a(n,n) will be destroyed 
+! during the calculation
+!===========================================================
+implicit none 
+integer n
+double precision a(n,n), c(n,n)
+double precision L(n,n), U(n,n), b(n), d(n), x(n)
+double precision coeff
+integer i, j, k
+
+! step 0: initialization for matrices L and U and b
+! Fortran 90/95 aloows such operations on matrices
+L=0.0
+U=0.0
+b=0.0
+
+! step 1: forward elimination
+do k=1, n-1
+   do i=k+1,n
+      coeff=a(i,k)/a(k,k)
+      L(i,k) = coeff
+      do j=k+1,n
+         a(i,j) = a(i,j)-coeff*a(k,j)
+      end do
+   end do
+end do
+
+! Step 2: prepare L and U matrices 
+! L matrix is a matrix of the elimination coefficient
+! + the diagonal elements are 1.0
+do i=1,n
+  L(i,i) = 1.0
+end do
+! U matrix is the upper triangular part of A
+do j=1,n
+  do i=1,j
+    U(i,j) = a(i,j)
+  end do
+end do
+
+! Step 3: compute columns of the inverse matrix C
+do k=1,n
+  b(k)=1.0
+  d(1) = b(1)
+! Step 3a: Solve Ld=b using the forward substitution
+  do i=2,n
+    d(i)=b(i)
+    do j=1,i-1
+      d(i) = d(i) - L(i,j)*d(j)
+    end do
+  end do
+! Step 3b: Solve Ux=d using the back substitution
+  x(n)=d(n)/U(n,n)
+  do i = n-1,1,-1
+    x(i) = d(i)
+    do j=n,i+1,-1
+      x(i)=x(i)-U(i,j)*x(j)
+    end do
+    x(i) = x(i)/u(i,i)
+  end do
+! Step 3c: fill the solutions x(n) into column k of C
+  do i=1,n
+    c(i,k) = x(i)
+  end do
+  b(k)=0.0
+end do
+end subroutine inverse
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+subroutine coeff(coeffs,n,k,l_y,x_vec)
+implicit none 
+integer, intent(in) :: n,k,x_vec(n)
+double precision,intent(in) :: l_y(n,1)
+double precision ::  x_mat(n,k), x_prod(k,k), x_inv(k,k),predict(k,1)
+double precision, intent(out) :: coeffs(k,1)
+integer :: I
+double precision :: linear(n)
+
+linear = (/ (I, I = 1, n) /)
+do I = 1,n
+ x_mat(I,1) = 1.
+end do
+x_mat(1:n,k) = x_vec
+ 
+x_prod = matmul(transpose(x_mat),x_mat)
+call inverse(x_prod,x_inv,k)
+
+predict =  matmul(transpose(x_mat),l_y)  
+
+coeffs = matmul(x_inv,predict)
+
+
+end subroutine coeff
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+subroutine linspace(z,x,y,n)
+    implicit none
+    
+    !n = the dimension of the output vector
+    !z = the n x 1 output vector, with equally spaced points between x and y
+    !x = the minimum of the linear grid
+    !y = the maximum of the linear grid
+    
+    
+    !input/output declarations
+    integer :: n
+    double precision :: z(n),x,y
+    
+    !local declarations
+    integer :: i
+    double precision :: d
+    
+    d = (y-x)/dble(n-1)
+    z(1) = x
+    
+    do i = 2,n-1
+        z(i) = z(i-1) + d
+    end do
+    
+    z(n) = y
+    
+    return
+
+end subroutine linspace
+  
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+double precision function erfcc(x)
+implicit none
+
+!input/output declarations
+double precision :: x
+
+!other declarations
+double precision :: t,z
+
+z = abs(x)
+t = 1.0 / (1.0 + 0.5*z)
+
+erfcc = t * exp(-z * z-1.26551223+t*(1.00002368+t*(0.37409196+&
+    t*(0.09678418+t*(-0.18628806+t*(0.27886807+t*(-1.13520398+&
+    t*(1.48851587+t*(-0.82215223+t*0.17087277)))))))))
+
+if (x.lt.0.0) erfcc = 2.0 - erfcc
+end function erfcc
+  
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+double precision function normcdf(x,mu,sigma)
+implicit none
+
+!input/output declarations
+!x: the input value for the normal cdf
+!mu: the mean of the normal distribution
+!sigma: the standard deviation of the normal distribution
+double precision :: x,mu,sigma
+
+!other declarations
+double precision :: z
+
+!standardized value ~ N(0,1)
+z = (x-mu)/sigma
+
+normcdf =  0.5 * erfcc( ( -1.0 * z ) / sqrt(2.0) )
+    
+end function normcdf
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+subroutine tauchen(znum,rhoz,sigmaz,nstdevz,pr_mat_z,z0)
+implicit none
+
+!input/output declarations
+integer, intent(in) :: znum
+double precision, intent(in) :: rhoz,sigmaz,nstdevz
+double precision, intent(out) :: pr_mat_z(znum,znum),z0(znum)
+    
+!other declarations
+integer :: zct,zprimect
+double precision :: zmin,zmax,gridinc,stdev
+
+!determine end points of the grid (log space)
+stdev =  ((sigmaz**2.0)/(1-rhoz**2.0))**0.5
+zmin = - nstdevz * stdev
+zmax = nstdevz * stdev
+
+!insert points into the grid (log space)
+call linspace(z0,zmin,zmax,znum)
+gridinc = z0(2)-z0(1)
+
+!loop over z states
+do zct=1,znum
+    
+    !insert transition matrix middle rows
+    do zprimect=2,(znum-1)
+        pr_mat_z(zct,zprimect) = &
+            normcdf(z0(zprimect)+gridinc/2.0,rhoz*z0(zct),sigmaz) - &
+            normcdf(z0(zprimect)-gridinc/2.0,rhoz*z0(zct),sigmaz)
+    end do !zct
+    
+    !first interval and last interval take the rest of the weight
+    pr_mat_z(zct,1) = normcdf(z0(1)+gridinc/2.0,rhoz*z0(zct),sigmaz)
+    pr_mat_z(zct,znum) = 1.0 - normcdf(z0(znum)-gridinc/2.0,rhoz*z0(zct),sigmaz)
+    
+end do !zct
+
+!round the transition matrix
+do zct=1,znum
+    pr_mat_z(zct,:) = pr_mat_z(zct,:)/sum(pr_mat_z(zct,:))
+end do !zct
+
+!convert grid back to z-space
+!z0 = exp(z0)
+    
+end subroutine tauchen
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+subroutine creategrid(lmax,bmax,stepl,stepb,vecsize,lgrid,bgrid)
+implicit none
+
+!input/output declarations
+integer, intent(in) :: vecsize
+double precision, intent(in) :: bmax,lmax,stepl,stepb
+double precision, intent(out) :: lgrid(vecsize,vecsize), bgrid(vecsize,vecsize)
+
+!other declarations
+double precision :: lvector(vecsize),bvector(vecsize)
+integer :: ii
+
+
+    call linspace(lvector,stepl,lmax,vecsize)
+    !vecsize = the dimension of the output vector
+    !lvector = the nx1 output vector, w/ equally spaced points b/w  x and y
+    !stepl  = the minimum of the linear grid
+    !lmax  = the maximum of the linear grid
+
+    do ii=1,vecsize
+       lgrid(:,ii)  = lvector
+    end do 
+
+    call linspace(bvector,stepb,bmax,vecsize)
+    !vecsize  = the dimension of the output vector
+    !bvector = the nx1 output vector, w/ equally spaced points b/w  x and y
+    !stepb  = the minimum of the linear grid
+    !bmax = the maximum of the linear grid
+
+    do ii=1,vecsize
+       bgrid(:,ii)  = bvector
+    end do
+
+    bgrid = transpose(bgrid)
+    
+  end subroutine creategrid
+
+
+
+subroutine qsimpweightsnodes(a,b,n,weights,nodes)
+implicit none
+
+!a = lower limit of integration
+!b = upper limit of integration
+!n = number of intervals, must be even.  There will be n+1 weights/nodes
+!weights = n+1 x 1 vector of integration weights
+!nodes = n+1 x 1 vector integration nodes
+
+!input/output declarations
+integer, intent(in)  :: n
+double precision, intent(in)  :: a,b
+double precision, intent(out)  :: weights(n+1),nodes(n+1)
+
+!other declarations
+integer :: ct
+
+weights(:) = 1.0
+
+do ct=1,n+1
+    nodes(ct) = a + ((b-a)/dble(n)) * (dble(ct)-1.0)
+end do !ct
+
+do ct=2,n
+    if (mod(ct,2)==0) then 
+        weights(ct) = 4.0
+    else 
+        weights(ct) = 2.0
+    end if
+end do !ct
+
+weights = weights * ( (b-a) / ( 3.0 * dble(n) ) )
+
+end subroutine qsimpweightsnodes
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 end module Aggregator
