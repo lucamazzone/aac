32b9ba39c346a03095a76410b002c2876aeef302
lucamazzone <luca.g.mazzone@gmail.com>
2017-04-16 18:41:17 +0200

    aggregatori

--- TasmanianSparseGrids/attempt/Aggregator.f95
+++ TasmanianSparseGrids/attempt/Aggregator.f95
@@ -1,34 +1,7 @@
 module Aggregator
 implicit none
 
-  double precision, parameter :: alpha = 0.7 !labor productivity
-  double precision, parameter :: beta = 0.985 ! hh discount factor => r = 2,67% 
-  double precision, parameter :: eta = 1 ! hh IES
-  double precision, parameter :: chi = 0.5  ! Fritsch labor elasticity
-  double precision, parameter :: gamma = 7.7 ! elasticity of subs b/w goods
-  double precision, parameter :: rhoz = 0.7 ! serial corr of idiosync shocks
-  double precision, parameter :: rhosigma = 0.85 ! serial corr of unc shocks
-  double precision, parameter :: kappa = 0.4 ! Jensen effect
-  double precision, parameter :: phi = 0.13 ! std of unc shocks
-  double precision, parameter :: musigma = 0.18 ! mean of unc process
-  double precision, parameter :: csi = 0.5 ! entry costs
-
-  double precision, parameter :: nstdevz = 1.0  ! stuff for tauchen
-
-  integer, parameter :: Zsize = 10 ! colsize of idiosync shock matrix
-  integer, parameter :: vecsize = 40 ! colsize of lgrid and bgrid
-  integer, parameter :: vecinterp = 150 ! size of interpolated version
-  integer, parameter :: snum = 2 ! high, low uncertainty
   
-  integer, parameter :: nsimp = 100 ! number of Simpson quadnodes (ntb even!)
-  integer, parameter :: momnum = 5 ! number of reference moments to be used
-
-  ! parameters for the grid of the firm problem
-
-  double precision, parameter :: lmax = 1.3871 ! maxval of labor grid
-  double precision, parameter :: bmax  = 0.5 ! maxval of debt grid
-  double precision, parameter :: stepl = lmax/vecsize ! distance b/w gridpoints
-  double precision, parameter :: stepb = bmax/vecsize ! distance b/w gridpoints
 
 ! change end program into end module at the end
 !use params
@@ -57,8 +30,34 @@ subroutine mapping_inverse(points,aggregate,pred)
 implicit none
 
 
-integer, parameter :: Zsize = 10
+  double precision, parameter :: alpha = 0.7 !labor productivity
+  double precision, parameter :: beta = 0.985 ! hh discount factor => r = 2,67% 
+  double precision, parameter :: eta = 1 ! hh IES
+  double precision, parameter :: chi = 0.5  ! Fritsch labor elasticity
+  double precision, parameter :: gamma = 7.7 ! elasticity of subs b/w goods
+  double precision, parameter :: rhoz = 0.7 ! serial corr of idiosync shocks
+  double precision, parameter :: rhosigma = 0.85 ! serial corr of unc shocks
+  double precision, parameter :: kappa = 0.4 ! Jensen effect
+  double precision, parameter :: phi = 0.13 ! std of unc shocks
+  double precision, parameter :: musigma = 0.18 ! mean of unc process
+  double precision, parameter :: csi = 0.5 ! entry costs
 
+  double precision, parameter :: nstdevz = 1.0  ! stuff for tauchen
+
+  integer, parameter :: Zsize = 10 ! colsize of idiosync shock matrix
+  integer, parameter :: vecsize = 40 ! colsize of lgrid and bgrid
+  integer, parameter :: vecinterp = 150 ! size of interpolated version
+  integer, parameter :: snum = 2 ! high, low uncertainty
+  
+  integer, parameter :: nsimp = 100 ! number of Simpson quadnodes (ntb even!)
+  integer, parameter :: momnum = 5 ! number of reference moments to be used
+
+  ! parameters for the grid of the firm problem
+
+  double precision, parameter :: lmax = 1.3871 ! maxval of labor grid
+  double precision, parameter :: bmax  = 0.5 ! maxval of debt grid
+  double precision, parameter :: stepl = lmax/vecsize ! distance b/w gridpoints
+  double precision, parameter :: stepb = bmax/vecsize ! distance b/w gridpoints
 
 !double precision, intent(in) :: points(2),mzero,Tol
 !integer, intent(in) :: aggregate
